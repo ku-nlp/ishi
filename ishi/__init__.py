fb0f01cb56a115d8c4ce5b1a483216c4f5af120e
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2019-12-21 17:30:32 +0900

    add a skelton

---
+++ ishi/__init__.py
@@ -0,0 +1,1 @@
+from .ishi import is_volition
---
+++ ishi/ishi.py
@@ -0,0 +1,13 @@
+"""Ishi: A volition classifier for Japanese."""
+
+
+def is_volition(input_str):
+    """Checks if the given input has volition.
+
+    Args:
+        input_str (str): An input string.
+
+    Returns:
+        bool: True for having volition, False otherwise.
+    """
+    raise NotImplementedError
